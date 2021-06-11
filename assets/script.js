b4248035f25b5b72c9c2909a9513029853daad11
Dana Bottoni <danabottoni@gmail.com>
2021-06-11 12:50:28 -0400

    countdown timer

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// var questions = [
+//   { q: "Commonly used data types DO NOT include", a: "answer3" },
+//   { q: "The condition in an if / else statement is enclosed with _______.", a: "1. parenthesis"}
+
+// ];
+
+
+// var questionElement = document.getElementById('question');
+// var answer1Element = document.getElementById('answer3');
+
+// for (var i = 0; i < questions.length; i++)   {
+//   questionElement.textContent = questions[i].q;
+//   answer1Element.textContent = questions[i].a;
+
+//   if ((answer === true && questions[i].a === "t") ||
+//       (answer === false && questions[i].a ==="f")) {
+//         score++;
+        
+//       }
+//       else{
+//         //alert("Wrong!");
+//       }
+//   }
+
+//    alert("You got " + score + "/" + questions.length);
+
+
+
+  
