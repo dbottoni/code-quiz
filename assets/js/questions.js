b2a2225852e3ff6c439017eeaa2054fb570c3618
Dana Bottoni <danabottoni@gmail.com>
2021-06-17 13:09:29 -0400

    changes

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+// // array to create questions 1/2
+// var questions = [
+//   new Question("Commonly used data types DO Not Include:", ["1. Strings", "2.Booleans", "3. Alerts", "4. Numbers"], "3. Alerts"),
+//   new Question("The condition in an if / else statement is enclosed with _____.", ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"], "3. Parenthesis"),
+//   new Question("Arrays in JavaScript can be used to store _______.", ["1. Numbers and Srings", "2. Other arrays", "3. Booleans", "4. All of The Above"], "4. All of The Above"),
+//   new Question("String values must be enclosed within ______ when being assigned to variables.", ["1.Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"], "4. Parenthesis"),
+//   new Question("A very useful tool use during development and debugging for printing content to the debugger is:", ["1. JavaScript", "2. Terminal / Bash", "3. For Loops", "4. console.log"], "4. console.log")
+// ];
+
+
+// questions array 2/2
+var questions = [
+  {
+    ques: "Commonly used data types DO NOT Include:",
+    choices: ["1. Strings", "2.Booleans", "3. Alerts", "4. Numbers"], 
+    answer: "3. Alerts"
+  },
+  {
+    ques:"The condition in an if / else statement is enclosed with _____.",
+    choices:["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"], 
+    answer: "3. Parenthesis"
+  },
+  {
+  ques:"Arrays in JavaScript can be used to store _______.", 
+  choices: ["1. Numbers and Srings", "2. Other arrays", "3. Booleans", "4. All of The Above"], 
+  answer: "4. All of The Above"
+  },
+  {
+  ques:"String values must be enclosed within ______ when being assigned to variables.",
+  choices: ["1.Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"], 
+  answer: "3. Quotes"
+  },
+  {
+  ques: "A very useful tool use during development and debugging for printing content to the debugger is:", 
+  choices: ["1. JavaScript", "2. Terminal / Bash", "3. For Loops", "4. console.log"], 
+  answer: "4. console.log"
+  },
+];
